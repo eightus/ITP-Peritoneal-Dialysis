6164d257ac6725c18097e1c1ede81f509919df81
bren37 <2200682@sit.singaporetech.edu.sg>
2024-06-17 21:13:51 +0800

    added add button

--- app/src/main/java/com/itp/pdbuddy/ui/screen/CurrentSuppliesScreen.kt
+++ app/src/main/java/com/itp/pdbuddy/ui/screen/CurrentSuppliesScreen.kt
@@ -5,12 +5,17 @@ import androidx.navigation.NavHostController
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.twotone.Inventory2
 import androidx.compose.material3.Button
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
@@ -21,21 +26,44 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.compose.material3.TopAppBarDefaults
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun CurrentSuppliesScreen(navController: NavHostController) {
 
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(16.dp),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
-    ) {
-        Text(
-            text = "Current Supplies",
-            fontSize = 24.sp,
-        )
-        // Add your content here
+    Scaffold (
+        modifier = Modifier.fillMaxSize(),
+    ) { values ->
+        Column (
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(values),
+
+        ) {
+            Row(
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(
+                    text = "Current Supplies",
+                    fontSize = 24.sp,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.weight(1f) // This makes the text take up remaining space
+                )
+                IconButton(
+                    onClick = {
+                        // Handle add button click
+                    }
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.Add,
+                        contentDescription = "Add"
+                    )
+                }
+            }
+            Spacer(modifier = Modifier.height(16.dp))
+            // Add your content here
+        }
     }
 }
