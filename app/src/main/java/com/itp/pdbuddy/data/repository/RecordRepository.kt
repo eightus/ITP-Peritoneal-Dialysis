5d75d9b139b765c8910ccd356947aea838a7a6b0
MelvinTanSK <114079325+MelvinTanSK@users.noreply.github.com>
2024-07-19 02:38:39 +0800

    Added Prescription to Auto Recording

--- app/src/main/java/com/itp/pdbuddy/data/repository/RecordRepository.kt
+++ app/src/main/java/com/itp/pdbuddy/data/repository/RecordRepository.kt
@@ -1,14 +1,16 @@
 package com.itp.pdbuddy.data.repository
 
+import com.itp.pdbuddy.data.model.Prescription
 import com.itp.pdbuddy.data.remote.APIDataSource
+import com.itp.pdbuddy.data.remote.PrescriptionDataSource
 import com.itp.pdbuddy.data.remote.RecordDataSource
 import com.itp.pdbuddy.utils.Result
 import javax.inject.Inject
 
 class RecordRepository @Inject constructor(
     private val recordDataSource: RecordDataSource,
-    private val apiDataSource: APIDataSource
-
+    private val apiDataSource: APIDataSource,
+    private val prescriptionDataSource: PrescriptionDataSource
 ) {
 
     suspend fun getRecords(name: String): Result<List<Map<String, Any>>> {
@@ -26,5 +28,9 @@ class RecordRepository @Inject constructor(
             apiDataSource.getWeight(),
             apiDataSource.getTimeOn(),
             apiDataSource.getTimeOff())
+    }
+
+    suspend fun getLatestPrescription(username: String): Result<Prescription> {
+        return prescriptionDataSource.getLatestPrescription(username)
     }
 }
